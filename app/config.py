e80bea7759b29cd4d9273afc1b4fab2ab02ea79d
marundu <marundu@gmail.com>
2018-02-28 11:48:58 +0300

    Initial commit + added SQLAlchemy usage

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+class Configuration(object):
+    DEBUG=True
+    #PORT=6702 # ?
+    APPLICATION_DIR=os.path.dirname(os.path.realpath(__file__))
+    SQLALCHEMY_DATABASE_URI='sqlite:///{}/blog.db'.format(APPLICATION_DIR)
+    # dialect+driver://username:password@host:port/database
+    # postgresql://postgres:password@localhost:5432/blog_db
